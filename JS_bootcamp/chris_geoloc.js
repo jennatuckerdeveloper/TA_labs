d02f37d316c3d7bc934b5f5e78ea519a4142cf47
JennaMTucker <jenna.m.tucker@gmail.com>
2017-11-01 11:22:09 -0700

    remove venv

--- JS_bootcamp/chris_geoloc.js
+++
@@ -1,9 +0,0 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-// This comes from JS global objects and built-in functions.
-
-function showPosition(pos) {
-    console.log(pos.coords.latitude, pos.coords.longitude);
-}
